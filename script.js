cabfc2fa267b655e7cb48de9d3c6644028eab93f
Michel Plainchamp <michel.plainchamp@gmail.com>
2022-04-08 22:26:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+const getAllUniqueTags = () => {
+	let uniqueTags = [...new Set(Array.from(document.querySelectorAll('*')).map((elt) => elt.tagName.toLowerCase()))]
+
+	return uniqueTags.sort()
+
+}
+
+
+const getAllUniqueAtributes = (tagNamesArray) =>{
+
+	let allAttributes = []
+
+	tagNamesArray.forEach((tag) => {
+
+		document.querySelectorAll(tag).forEach((tagInstance) => {
+
+			allAttributes = [...allAttributes, ...tagInstance.getAttributeNames()]
+			// console.log(tagInstance.getAttributeNames())
+
+			// allAttributes = [allAttributes, ...tagInstance.getAttributeNames()]
+
+		})
+	})
+
+	return [...new Set(allAttributes)]
+
+}
+
+
+
+
+const getArrayOfTagsAndUniqueAttributes = (tagNamesArray) => {
+
+	return uniqueTags.map((tagName) => {
+
+		let arrayOfAttributesPerTag = []
+
+		document.querySelectorAll(tagName).forEach((tag) => {
+			arrayOfAttributesPerTag = [...arrayOfAttributesPerTag, ...tag.getAttributeNames()]
+		})
+
+		const arrayOfUniqueAttributesPerTag = [...new Set(arrayOfAttributesPerTag)]
+
+		return {tag: tagName, attributes: arrayOfUniqueAttributesPerTag}
+	})
+
+}
+
+
+
+///////////////////
+
+const uniqueTags = getAllUniqueTags()
+
+const bigArray = getArrayOfTagsAndUniqueAttributes(uniqueTags)
+
+console.log(bigArray)
